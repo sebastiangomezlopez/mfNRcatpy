17b7284c583c1e9558083607380720d0794544a2
sebastian <segomezlo@unal.edu.co>
2023-03-27 23:28:26 +0200

    first packaged  version of the code, ready to install with pip

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='mfNRcatpy',
+    version='0.0.1',
+    install_requires=['numpy',
+                      'scipy',
+                      'core-watpy',
+                      'h5py',
+                      'matplotlib',
+                      'lalsuite',
+                      'numba'],
+    author = 'Sebastian Gomez Lopez',
+    author_email= 'segomezlo@unal.edu.co',
+    url = 'github.com/gththcoc/mfNRcatpy'
+
+    )
